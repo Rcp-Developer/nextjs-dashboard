4be0992987d52eabde06e45b2cd29414f1c20df9
Rodrigo-agrojet <rodrigo-q2@hotmail.com>
2024-10-18 11:29:00 -0300

    - Fru fruu

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,74 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
-import Image from 'next/image';
+'use client'
+
+//import AcmeLogo from '@/app/ui/acme-logo';
+//import { ArrowRightIcon } from '@heroicons/react/24/outline';
+//import Link from 'next/link';
+import { loveLight } from '@/app/ui/fonts';
+import { roboto } from '@/app/ui/fonts';
+//import Image from 'next/image';
+import { useEffect } from 'react';
+import confetti from 'canvas-confetti';
+import { motion } from 'framer-motion';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
+  useEffect(() => {
+    // Função para fogos de artifício ocupar toda a tela
+    const fireConfetti = () => {
+      var end = Date.now() + 3 * 1000; // Duração de 3 segundos
+
+      (function frame() {
+        confetti({
+          particleCount: 3,
+          angle: 60,
+          spread: 80,
+          origin: { x: 0 },
+          colors: ['#ff0000', '#ffffff', '#ff69b4'],
+        });
+        confetti({
+          particleCount: 3,
+          angle: 120,
+          spread: 80,
+          origin: { x: 1 },
+          colors: ['#ff0000', '#ffffff', '#ff69b4'],
+        });
+
+        if (Date.now() < end) {
+          requestAnimationFrame(frame);
+        }
+      })();
+    };
+
+    fireConfetti();
+  });
+
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-          src="/hero-desktop.png"
-          width={1000}
-          height={760}
-          className='hidden md:block'
-          alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-          src="/hero-mobile.png"
-          width={560}
-          height={620}
-          className='block md:hidden'
-          alt="Screenshots of the dashboard project showing desktop version"
-          />
-        </div>
+    <main className={loveLight.className}>
+      <div className={styles.container}>
+        <div className={styles.heartsBackground}></div>
+        <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.5 }}
+          className={`${styles.title} ${loveLight.className}`}
+        >
+          Eliane Ramos de Queiroz, você é o amor da minha vida!
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1, duration: 2 }}
+          className={`${styles.message} ${loveLight.className}`}
+        >
+          Eu sou muito privilegiado por ter você ao meu lado. Te amo muito minha Paraibana linda e goxtosaaa! 🌵😍👩‍❤️‍👨
+        </motion.p>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1, duration: 2.5 }}
+          className={`${styles.submessage} ${loveLight.className}`}>
+          Do seu eterno admirador e namorado apaixonado, <b className={roboto.className}>Rodrigo Coladello Pereira ❤️👨‍💻</b>
+        </motion.p>
       </div>
     </main>
   );
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,6 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Love_Light, Roboto } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ['latin'] });
+export const loveLight = Love_Light({ weight: "400", subsets: ['latin'] });
+export const roboto = Roboto({ weight: "400", subsets: ['latin'] });
